ad6b6bb39b3b08ee30ffb9250b35e36722ad0c03
Abed <abed@abeds.local>
2017-01-20 11:07:39 +0100

    Basic Login UI created

---
+++ Social App/FancyView.swift
@@ -0,0 +1,14 @@
+//
+//  FancyView.swift
+//  Social App
+//
+//  Created by Abed on 19/01/2017.
+//  Copyright © 2017 Webiaturist. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+
+}
